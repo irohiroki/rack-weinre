76d6561ae2b8b0f666c20f879c28fa256ab5dbb2
Hiroki Yoshioka <irohiroki@gmail.com>
2012-04-01 02:09:01 +0900

    core implementation.

--- lib/rack/weinre.rb
+++ lib/rack/weinre.rb
@@ -5,7 +5,32 @@ module Rack
     end
 
     def call(env)
-      @app.call(env)
+      res = @app.call(env)
+      if res[1]['Content-Type'] =~ /text\/html/ && enabled
+        res.tap do |res|
+          orig = res[2]
+          res[2] = [].tap do |body|
+            orig.each do |line|
+              body << line.sub(/(?=<\/head>)/, tag(env))
+            end
+            orig.close if orig.respond_to?(:close)
+          end
+        end
+      else
+        res
+      end
+    end
+
+    def enabled
+      @options[:switch].nil? || ::File.exist?(Pathname.pwd + @options[:switch])
+    end
+
+    def port
+      @options[:port] || 8080
+    end
+
+    def tag(env)
+      %{<script src="http://#{env['SERVER_NAME']}:#{port}/target/target-script-min.js#anonymous"></script>\n}
     end
   end
 end
--- spec/rack/weinre_spec.rb
+++ spec/rack/weinre_spec.rb
@@ -2,11 +2,43 @@ require 'spec_helper'
 
 describe Rack::Weinre do
   before do
-    rackup
+    rackup app, options
+    visit '/'
   end
 
-  it "passes through" do
-    visit '/'
-    page.should have_css 'html head'
+  subject { page }
+
+  let(:app){ stub_app }
+  let(:options){ {} }
+
+  context "when response type is text/html" do
+    it { should have_css 'head script[src="http://www.example.com:8080/target/target-script-min.js#anonymous"]' }
+  end
+
+  context "when response type is not text/html" do
+    let(:app){ stub_app(:headers => {'Content-Type' => 'text/plain'}) }
+    it { should_not have_css 'head script'}
+  end
+
+  context "with content type with charset" do
+    let(:app){ stub_app(:headers => {'Content-Type' => 'text/html; charset=utf-8'}) }
+    it { should have_css 'head script'}
+  end
+
+  context "with a :port option" do
+    let(:options){ {:port => '8765'} }
+    its(:source) { should match 'www.example.com:8765' }
+  end
+
+  context "with a :switch option" do
+    context "refering a non-existent file" do
+      let(:options){ {:switch => 'bogus'} }
+      it { should_not have_css 'head script'}
+    end
+
+    context "refering a existent file" do
+      let(:options){ {:switch => 'Gemfile'} }
+      it { should have_css 'head script'}
+    end
   end
 end
